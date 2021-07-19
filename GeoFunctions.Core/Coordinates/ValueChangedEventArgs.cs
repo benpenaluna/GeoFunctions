bf0739abdbf0cee911be915aa8ac546d47705475
Ben Penaluna <penalunaben@gmail.com>
2021-07-19 20:25:01 +1000

    Created Custom ValueChangedEventArgs

---
+++ GeoFunctions.Core/Coordinates/ValueChangedEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeoFunctions.Core.Coordinates
+{
+    public class ValueChangedEventArgs : EventArgs
+    {
+        public double NewValue { get; set; }
+        public double OldValue { get; set; }
+        
+        public ValueChangedEventArgs() : base() { }
+    }
+}
